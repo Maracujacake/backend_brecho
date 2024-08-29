fd3db5bd888681c6ed51fc1035ae3b98c261b86a
Christian Polli <madlordx@gmail.com>
2024-08-29 20:21:15 -0300

    Primeiro commit

---
+++ src/main/java/hackaton/brecho/BrechoApplication.java
@@ -0,0 +1,13 @@
+package hackaton.brecho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrechoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrechoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hackaton/brecho/BrechoApplicationTests.java
@@ -0,0 +1,13 @@
+package hackaton.brecho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrechoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
